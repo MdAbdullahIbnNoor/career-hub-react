880e6eca127c99070700a97e54a3b4cd2ffa6e77
MdAbdullahIbnNoor <ab.89noor@gmail.com>
2023-09-22 12:14:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-4xl text-center font-extrabold text-emerald-600'>Vite + React</h1>
+
+      <button className='btn btn-accent text-white'>Click Me</button>
+      
+    </>
+  )
+}
+
+export default App
